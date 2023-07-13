133891a8e6f5d6ecf66f905fca9ed75df4885f3a
DeaNo409 <107428738+DeaNo409@users.noreply.github.com>
2023-07-14 00:03:23 +0200

    Update s99_fix_soundscapes.lua

--- Site_99_sound_fix/lua/autorun/s99_fix_soundscapes.lua
+++ Site_99_sound_fix/lua/autorun/s99_fix_soundscapes.lua
@@ -1,3 +1,5 @@
+-- Bereitgestellt von DeaNo mithilfe von Blubberblase :)
+
 if ( game.GetMap() != "rp_site99_v3" ) then return end
 
 hook.Add( "PlayerInitialSpawn", "wither_reset_sounds", function( ply )
