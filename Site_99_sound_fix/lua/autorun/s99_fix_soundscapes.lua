677d3b36aef508e7937ba6ee87050d34eac54bd9
DeaNo409 <107428738+DeaNo409@users.noreply.github.com>
2023-07-14 00:01:01 +0200

    Update s99_fix_soundscapes.lua

--- Site_99_sound_fix/lua/autorun/s99_fix_soundscapes.lua
+++ Site_99_sound_fix/lua/autorun/s99_fix_soundscapes.lua
@@ -3,3 +3,4 @@ if ( game.GetMap() != "rp_site99_v3" ) then return end
 hook.Add( "PlayerInitialSpawn", "wither_reset_sounds", function( ply )
 	ply:ConCommand( "snd_restart" )
 end )
+print("[Site_99_Sound_Fix] Sound fix geladen!") -- Print in der Konsole :D
